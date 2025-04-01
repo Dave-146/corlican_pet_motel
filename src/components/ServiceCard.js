34687e9d370d67bccb96371da0bb0253ca0e1cf9
Your Name <your.email@example.com>
2025-03-31 20:06:53 -0400

    colour change

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -61,7 +61,7 @@ export default function ServiceCard({
             ))}
           </ul>
           <button 
-            className="bg-dark text-white px-6 py-3 rounded-md hover:bg-primary transition-colors duration-200 w-full text-center block font-sans"
+            className="text-secondary text-white px-6 py-3 rounded-md hover:bg-primary transition-colors duration-200 w-full text-center block font-sans"
             onClick={(e) => {
               e.stopPropagation();
               setIsExpanded(true);
